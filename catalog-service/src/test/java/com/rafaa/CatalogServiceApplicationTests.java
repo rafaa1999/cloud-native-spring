caea26c277272e4c74482353c6662278325151e5
rafaa1999 <raferhouma10@gmail.com>
2024-07-23 16:19:06 +0100

    Setup catalog-service

---
+++ catalog-service/src/test/java/com/rafaa/CatalogServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rafaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CatalogServiceApplicationTests {
+    @Test
+    void set_up(){
+
+    }
+}
---
+++ src/main/java/com/rafaa/Main.java
@@ -0,0 +1,6 @@
+package com.rafaa;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
