21622bf68b61c826085617dcc7234977a6f23201
rafaa1999 <raferhouma10@gmail.com>
2024-07-24 12:15:23 +0100

    Write tests for BookControllerMvcTests:
    . when get book not existing then should return 404
    . should complete the other tests later!!

---
+++ catalog-service/src/test/java/com/rafaa/catalog_service/web/BookControllerMvcTests.java
@@ -0,0 +1,33 @@
+package com.rafaa.catalog_service.web;
+
+import com.rafaa.catalog_service.domain.BookNotFoundException;
+import com.rafaa.catalog_service.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void when_get_book_not_existing_then_should_return_404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+
+}
